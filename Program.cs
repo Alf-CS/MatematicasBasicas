181961c5fbb88f56620a610670cb6b395a69df4a
Alf-CS <116028213+Alf-CS@users.noreply.github.com>
2023-02-08 21:35:11 +0100

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Matematicas { 
+    class OperacionesBasicas{
+        public static void Main(String[] args){
+            int numeroA, numeroB, resultado;
+            Console.WriteLine("Introduce el primer número: ");
+            numeroA = int.Parse(Console.ReadLine());
+            Console.WriteLine("Introduce el segundo número: ");
+            numeroB = int.Parse(Console.ReadLine());
+            resultado = numeroA + numeroB;
+            Console.WriteLine("LA SUMA ES: " + numeroA + "+" + numeroB + " =" + resultado);
+            resultado = numeroA - numeroB;
+            Console.WriteLine("LA RESTA ES: " + numeroA + "-" + numeroB + " =" + resultado);
+            resultado = numeroA * numeroB;
+            Console.WriteLine("LA MULTIPLICACIÓN ES: " + numeroA + "*" + numeroB + " =" + resultado);
+            resultado = numeroA / numeroB;
+            Console.WriteLine("LA DIVISIÓN ES: " + numeroA + "/" + numeroB + " =" + resultado);
+            resultado = numeroA % numeroB;
+            Console.WriteLine("EL RESTO ES: " + numeroA + "&" + numeroB + " =" + resultado);
+        }
+    }
+}
